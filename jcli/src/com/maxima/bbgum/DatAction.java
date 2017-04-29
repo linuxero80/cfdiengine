c2388ace2e137ccb55332edd9dbbeaad996e28e6
j2eeserver <j2eeserver@server01>
2017-04-29 19:29:28 -0400

    jcli: added robustness to DatAction data type
    Signed-off-by: j2eeserver <j2eeserver@server01>

--- jcli/src/com/maxima/bbgum/DatAction.java
+++ jcli/src/com/maxima/bbgum/DatAction.java
@@ -6,6 +6,23 @@ public class DatAction {
     private byte transaction;
     private byte[] data;
 
+    public DatAction(final byte[] data) {
+
+        if (data.length > DatFrame.DAT_FRAME_BODY_MAX_LENGTH) {
+            // we should throw an exception here
+            System.out.println(" DatAction can not be bigger than " + DatFrame.SDU_FRAME_BODY_MAX_LENGTH + " " + "bytes");
+        }
+
+        this.setId(data[0]);
+        this.setTransaction(data[1]);
+
+        byte dataForAction[] = new byte[data.length - DatFrame.DAT_ACTION_FLOW_INFO_SEGMENT_LENGTH];
+        System.arraycopy(data,
+            DatFrame.DAT_ACTION_FLOW_INFO_SEGMENT_LENGTH,
+            dataForAction, 0, dataForAction.length);
+        this.setData(dataForAction);
+    }
+
     public byte getId() {
         return id;
     }
