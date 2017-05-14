db274acc74c9626fb256201987587bcf855c639c
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-14 16:33:21 -0500

    api: bbgum server is ready to be molded
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

--- api/service.py
+++ api/service.py
@@ -1,6 +1,45 @@
 from custom.profile import ProfileReader
 import os
 
+
+class BbGumServer(object):
+    def __init__(self, hostname, port, logger):
+        self.logger = logger
+        self.hostname = hostname
+        self.port = port
+
+    def getup(self):
+        self.logger.debug("listening")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.hostname, self.port))
+        self.socket.listen(1)
+
+        while True:
+            conn, address = self.socket.accept()
+            self.logger.debug("Got connection")
+            process = multiprocessing.Process(
+                target=self.handle, args=(conn, address))
+            process.daemon = True
+            process.start()
+            self.logger.debug("Started process %r", process)
+
+    def handle(self, connection, address):
+        try:
+            self.logger.debug("Connected %r at %r", connection, address)
+            while True:
+                data = connection.recv(1024)
+                if data == "":
+                    self.logger.debug("Socket closed remotely")
+                    break
+                self.logger.debug("Received data %r", data)
+                connection.sendall(data)
+                self.logger.debug("Sent data")
+        except:
+            self.logger.exception("Problem handling request")
+        finally:
+            self.logger.debug("Closing socket")
+            connection.close()
+
 class CfdiEngineError(Exception):
     def __init__(self, message = None):
         self.message = message
