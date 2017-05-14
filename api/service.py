c9392dd1240e3005ffcd2ad925e4d4dfb6969280
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-14 18:04:44 -0500

    api: fixed typos
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

--- api/service.py
+++ api/service.py
@@ -4,27 +4,63 @@ import multiprocessing
 import socket
 import os
 
+class CfdiEngineError(Exception):
+    def __init__(self, message = None):
+        self.message = message
+    def __str__(self):
+        return self.message
 
-class BbGumServer(object):
-    def __init__(self, hostname, port, logger):
+class CfdiEngine(object):
+
+    __HOST = '' # Symbolic name meaning all available interfaces
+
+    def __init__(self, logger, config_prof, port):
         self.logger = logger
-        self.hostname = hostname
         self.port = port
 
-    def getup(self):
-        self.logger.debug("listening")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.hostname, self.port))
-        self.socket.listen(1)
+        try:
+            reader = ProfileReader(self.logger)
+            proftree = reader(config_prof)
+        except:
+            msg = 'Problems came up when reading configuration profile'
+            raise CfdiEngineError(msg)
 
-        while True:
-            conn, address = self.socket.accept()
-            self.logger.debug("Got connection")
-            process = multiprocessing.Process(
-                target=self.handle, args=(conn, address))
-            process.daemon = True
-            process.start()
-            self.logger.debug("Started process %r", process)
+
+    def start(self):
+        """start the service upon selected port"""
+
+        def spawner():
+            self.logger.debug("listening")
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((self.hostname, self.port))
+            self.socket.listen(1)
+
+            while True:
+                conn, address = self.socket.accept()
+                self.logger.debug("Got connection")
+                process = multiprocessing.Process(
+                    target=self.handle, args=(conn, address))
+                process.daemon = True
+                process.start()
+                self.logger.debug("Started process %r", process)
+
+        def shutdown():
+            self.logger.info("Shutting down")
+            for process in multiprocessing.active_children():
+                self.logger.info("Shutting down process %r", process)
+                process.terminate()
+                process.join()
+
+        try:
+            print('Use Control-C to exit')
+            spawner()
+        except KeyboardInterrupt:
+            print('Exiting')
+        except BbGumServerError as e:
+            self.logger.error(e)
+            raise
+        finally:
+            shutdown()
 
     def handle(self, connection, address):
         try:
@@ -42,36 +78,3 @@ class BbGumServer(object):
         finally:
             self.logger.debug("Closing socket")
             connection.close()
-
-class CfdiEngineError(Exception):
-    def __init__(self, message = None):
-        self.message = message
-    def __str__(self):
-        return self.message
-
-class CfdiEngine(object):
-
-    __HOST = ''    # Symbolic name meaning all available interfaces
-
-    def __init__(self, logger, config_prof, port):
-        self.logger = logger
-
-        try:
-            reader = ProfileReader(self.logger)
-            proftree = reader(config_prof)
-        except:
-            msg = 'Problems came up when reading configuration profile'
-            raise CfdiEngineError(msg)
-
-        self.server = BbGumServer(self.__HOST, port, self.logger)
-
-    def start(self):
-        """start the service upon selected port"""
-        try:
-            print('Use Control-C to exit')
-            self.server.getup()
-        except KeyboardInterrupt:
-            print('Exiting')
-        except BbGumServerError as e:
-            self.logger.error(e)
-            raise
