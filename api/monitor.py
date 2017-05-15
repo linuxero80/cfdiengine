236b2d3d73a96e59bdb7d1354b4acf6d4bbf0b17
The Edward Nygma <j4nusx@yahoo.com>
2017-05-14 22:23:21 -0500

    api: added monitor class
    Signed-off-by: The Edward Nygma <j4nusx@yahoo.com>

---
+++ api/monitor.py
@@ -0,0 +1,9 @@
+class Monitor(object):
+
+    TRANSACTION_NUM_START_VALUE = 2
+    TRANSACTION_NUM_LAST_VALUE = 254
+    TRANSACTION_NUM_INCREMENT = 2
+    MAX_NODES = 256
+
+    def __init__(self):
+        pass
