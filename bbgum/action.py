1ecc7008b03966e56dfe68f191e480a6f28fe1ec
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-19 18:52:07 -0500

    bbgum: fixed typos
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

--- bbgum/action.py
+++ bbgum/action.py
@@ -1,6 +1,16 @@
+from bbgum.frame import FrameError
+
 class Action(object):
 
-    def __init__(self):
-        self.id = b'\x00'
-        self.transnum = b'\x00'
-        self.data = bytearray()
+    def __init__(self, data = None):
+        if data:
+            if (len(data) > Frame.FRAME_BODY_MAX_LENGTH):
+                msg = "Action can not be bigger than " + str(Frame.FRAME_BODY_MAX_LENGTH) + " " + "bytes";
+                raise FrameError(msg)
+
+            self.archetype = (data[0])
+            self.transnum = (data[1])
+        else:
+            self.archetype = b'\x00'
+            self.transnum = b'\x00'
+            self.buffer = bytearray()
