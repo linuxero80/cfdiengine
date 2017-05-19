a8d1f32292ece97b8ebfe72f76d86f45486d8dcb
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-19 18:16:58 -0500

    bbgum: fixed typos
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

--- bbgum/frame.py
+++ bbgum/frame.py
@@ -1,3 +1,9 @@
+class FrameError(Exception):
+    def __init__(self, message = None):
+        self.message = message
+    def __str__(self):
+        return self.message
+
 class Frame(object):
 
     FRAME_HEADER_LENGTH = 4
@@ -17,9 +23,11 @@ class Frame(object):
         body = byteszero(self.FRAME_BODY_MAX_LENGTH)
 
     @staticmethod
-    def encode_header(action_length):
+    def encode_header(length):
+        if length > Frame.FRAME_BODY_MAX_LENGTH:
+            raise FrameError("invalid length to encode!!")
         l = []
-        for sc in '{:3d}'.format(action_length):
+        for sc in '{:3d}'.format(length):
             l.append(ord(sc))
         l.append(Frame.C_NULL_CHARACTER)
         return bytes(l)
@@ -28,4 +36,8 @@ class Frame(object):
     def decode_header(header):
         cut_nullchar = lambda s: s[0:-1]
         if (len(header) == Frame.FRAME_HEADER_LENGTH):
-            return int(cut_nullchar(header).decode("utf-8"))
+            try:
+                return int(cut_nullchar(header).decode("utf-8"))
+            except:
+                raise FrameError("unexpected problems when decoding header!!")
+        raise FrameError("header's width violiation!!")
