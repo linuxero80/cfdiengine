62afd99d7492a1a7b32db7cb7e73498947d31fbb
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-19 19:34:12 -0500

    bbgum: merged action within frame class file
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

--- bbgum/frame.py
+++ bbgum/frame.py
@@ -42,3 +42,21 @@ class Frame(object):
             except:
                 raise FrameError("unexpected problems when decoding header!!")
         raise FrameError("header's width violiation!!")
+
+
+class Action(object):
+
+    self.archetype = b'\x00'
+    self.transnum = b'\x00'
+    self.buffer = bytearray()
+
+    def __init__(self, data = None):
+        byteszero = lambda n: bytearray([0] * n)
+        if data:
+            length = len(data)
+            if (length) > Frame.FRAME_BODY_MAX_LENGTH):
+                msg = "Action can not be bigger than " + str(Frame.FRAME_BODY_MAX_LENGTH) + " " + "bytes";
+                raise FrameError(msg)
+            self.archetype = (data[0])
+            self.transnum = (data[1])
+            self.buffer = data[Frame.ACTION_FLOW_INFO_SEGMENT_LENGTH:-1]
