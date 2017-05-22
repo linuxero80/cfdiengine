5fe03a5d9cf1a65ae7581ce7c87d02c7c378a30f
The Edward Nygma <j4nusx@yahoo.com>
2017-05-21 21:34:42 -0500

    bbgum: added controllers folder
    Signed-off-by: The Edward Nygma <j4nusx@yahoo.com>

---
+++ controllers/srhello.py
@@ -0,0 +1,13 @@
+from bbgum.controller import Sr
+
+class SrHello(Sr):
+    '''
+    Deals with single recive transaction's actions
+    '''
+    def __init__(self):
+        pass
+
+    def process_buff(buff):
+        rc = 0
+        buff.decode("utf-8")
+        return rc
---
+++ controllers/srpostbuff.py
@@ -0,0 +1,12 @@
+from bbgum.controller import Sr
+
+class SrPostBuff(Sr):
+    '''
+    Deals with single recive transaction's actions
+    '''
+    def __init__(self):
+        pass
+
+    def process_buff(buff):
+        rc = 0
+        return rc
