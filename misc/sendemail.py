fa6307b992a84c36caf9b9d58182d3b05b6b8731
j2eeserver <j2eeserver@aspire.plauchu.lan>
2017-05-27 02:33:38 -0500

    misc: added sendemail.py
    This module allows to send an email with just one attachment. It is very handy for send a document after a transaction
    
    Signed-off-by: j2eeserver <j2eeserver@aspire.plauchu.lan>

---
+++ misc/sendemail.py
@@ -0,0 +1,78 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
+
+class SendEmail(object):
+    '''
+    Function object that sends email with attachment
+    '''
+
+    # Following code was taken from
+    # http://naelshiab.com/tutorial-send-email-python
+
+    __DEFAULT_FROMADDR = 'johndove@localhost'
+    __DEFAULT_SMTP = 'localhost'
+    __DEFAULT_PORT = 587
+    __DEFAULT_PASSWD = ''
+
+    def __init__(self, logger, **kwargs):
+        self.logger = logger
+        self.server_config = {
+            'HOST': kwargs.get('host', self.__DEFAULT_SMTP),
+            'PORT': kwargs.get('port', self.__DEFAULT_PORT),
+            'PASSWD': kwargs.get('passwd', self.__DEFAULT_PASSWD)
+        }
+
+    def __call__(self, **kwargs):
+        '''Sends email with attachment'''
+
+        def encode_file():
+            '''Encodes file part'''
+            filename = os.path.basename(self.msg_config['ATTACHMENT'])
+            attachment = open(self.msg_config['ATTACHMENT'], "rb")
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment).read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+            return part
+
+        self.msg_config = {
+            'FROMADDR': kwargs.get('fromaddr', self.__DEFAULT_FROMADDR),
+            'TOADDR': kwargs.get('toaddr', None),
+            'SUBJECT': kwargs.get('subject', None),
+            'MSG': kwargs.get('msg', None),
+            'ATTACHMENT': kwargs.get('attachment', None)
+        }
+        m = self.__conform_body(MIMEMultipart())
+        m.attach(encode_file())
+        self.__dialog(m)
+
+    def __conform_body(self, m):
+        '''Conforms message body'''
+
+        m['From'] = self.msg_config['FROMADDR']
+        m['To'] = self.msg_config['TOADDR']
+        m['Subject'] = self.msg_config['SUBJECT']
+        body = self.msg_config['MSG']
+        m.attach(MIMEText(body, 'plain'))
+        return m
+
+    def __dialog(self, m):
+        '''Dialogs with smtp server'''
+        server = smtplib.SMTP(
+            self.server_config['HOST'],
+            self.server_config['PORT']
+        )
+        server.starttls()
+        server.login(
+            self.msg_config['FROMADDR'],
+            self.server_config['PASSWD']
+        )
+        text = m.as_string()
+        server.sendmail(
+            self.msg_config['FROMADDR'], 
+            self.msg_config['TOADDR'], 
+            text
+        )
+        server.quit()
