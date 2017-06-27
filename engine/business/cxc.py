6a84b1d8e034a60f8247fe0fb295cb1f8920a2c6
Bob Ross <pianodaemon@gmail.com>
2017-06-26 22:02:06 -0500

    engine: added docmaker instance to factura handler in cxc
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- engine/business/cxc.py
+++ engine/business/cxc.py
@@ -1,3 +1,22 @@
+from docmaker.pipeline import DocPipeLine
+from misc.tricks import dump_exception
+from engine.error import ErrorCode
+
 
 def facturar(logger, req):
-    return 0
+
+    def dm_exec(usr_id, prefact_id):
+        dm_builder = 'facxml'
+        kwargs = {'usr_id': usr_id, 'prefact_id': prefact_id}
+        try:
+            dpl = DocPipeLine(logger, resdir,
+                rdirs_conf = pt.res.dirs,
+                pgsql_conf = pt.dbms.pgsql_conn)
+            dpl.run(dm_builder, args.dm_output, **kwargs)
+            return ErrorCode.SUCCESS
+        except:
+            logger.error(dump_exception)
+            return ErrorCode.DOCMAKER_ERROR
+
+    rc = dm_exec(req.get(usr_id, None), req.get(prefact_id, None))
+    return rc.value
