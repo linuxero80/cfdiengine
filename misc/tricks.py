7ca6638849dab7da2e952245ab7ed705740bf526
Bob Ross <pianodaemon@gmail.com>
2017-06-30 13:37:18 -0500

    misc: fixed typos

--- misc/tricks.py
+++ misc/tricks.py
@@ -1,6 +1,7 @@
 import sys
 import traceback
 
+
 def dump_exception():
     exc_type, exc_value, exc_traceback = sys.exc_info()
     lines = traceback.format_exception(
@@ -10,15 +11,17 @@ def dump_exception():
     )
     return ''.join('!! ' + line for line in lines)
 
-def dict_params(l, k, v): 
-    '''
+
+def dict_params(l, k, v):
+    """
     creates a dictionary from a list of
     dictionaries with name/value elements
-    '''
+    """
     n = {}
     for d in l:
         n[d[k]] = d[v]
     return n
+
 
 def truncate(f, n):
     """
@@ -28,4 +31,4 @@ def truncate(f, n):
     if 'e' in s or 'E' in s:
         return '{0:.{1}f}'.format(f, n)
     i, p, d = s.partition('.')
-    return '.'.join([i, (d+'0'*n)[:n]])
+    return '.'.join([i, (d + '0' * n)[:n]])
