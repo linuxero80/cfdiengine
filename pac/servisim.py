c68f6c2b492c28a40d99d429c49c0bfadfaa78d7
Bob Ross <pianodaemon@gmail.com>
2017-07-01 18:10:09 -0500

    pac: fixed typos
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- pac/servisim.py
+++ pac/servisim.py
@@ -1,19 +1,19 @@
-from pac.adapter import Adapter
+from pac.adapter import Adapter, AdapterError
 from suds.client import Client
 from suds.transport import TransportError
 import urllib.error
+
 
 class Servisim(Adapter):
     """
     Current WS API of PAC Servisim
     """
     __PAC_DESC = 'Servisim - Facturacion Electronica'
-    __DEFAULT_EP = 'http://201.150.37.20/wstest/CFDI.svc?wsdl' #testing
 
-    def __init__(self, logger, *args, **kwargs):
+    def __init__(self, logger, **kwargs):
         super().__init__(logger, self.__PAC_DESC)
         self.config = {
-            'EP': kwargs.get('end_point', self.__DEFAULT_EP),
+            'EP': kwargs.get('end_point', None),
             'LOGIN': kwargs.get('login', None),
             'PASSWD': kwargs.get('passwd', None),
             'RFC': kwargs.get('rfc', None)
@@ -25,13 +25,14 @@ class Servisim(Adapter):
             try:
                 conn = Client(self.config['EP'])
                 self.logger.debug(
-                "{0} adapter is up and ready to kick buttocks\n{1}".format(
-                    self.pac_name, self.conn))
+                    "{0} adapter is up and ready to kick buttocks\n{1}".format(
+                        self.pac_name, self.conn))
                 return conn
             except (TransportError, urllib.error.HTTPError) as e:
                 self.logger.fatal(e)
                 raise AdapterError(
                     'can not connect with end point{0}'.format(self.ep))
+
         conn = connect()
         req = conn.factory.create(m)
         req.User = self.config['LOGIN']
@@ -39,15 +40,15 @@ class Servisim(Adapter):
         return req, conn
 
     def stamp(self, xml, xid):
-        '''
+        """
         Timbrado usando XML firmado por el cliente
         Args:
             xml (str): xml de cfdi firmado por cliente
             xid (str): mi identificador alternativo de cfdi
-        '''
+        """
         try:
             req, conn = self.__setup_req('ns0:TimbradoCFDIRequest')
-            req.TipoPeticion = '1' # SIGNED BY CUSTOMER
+            req.TipoPeticion = '1'  # SIGNED BY CUSTOMER
             req.IdComprobante = xid
             req.Xml = xml
             self.logger.debug(
@@ -58,15 +59,15 @@ class Servisim(Adapter):
             raise AdapterError("Stamp experimenting problems")
 
     def fetch(self, xid):
-        '''
+        """
         Obtencion de cfdi previamente timbrado mediante
         identificador de cfdi
         Args:
             xid (str): mi identificador alternativo de cfdi
-        '''
+        """
         try:
             req, conn = self.__setup_req('ns0:ObtencionCFDIRequest')
-            req.TipoPeticion = '2' # TO EXPECT CFDI ALTERNATIVE ID
+            req.TipoPeticion = '2'  # TO EXPECT CFDI ALTERNATIVE ID
             req.Emisor = self.config['RFC']
             req.Identificador = xid
             self.__logger.debug(
@@ -77,14 +78,14 @@ class Servisim(Adapter):
             raise AdapterError("Fetch experimenting problems")
 
     def cancel(self, xml):
-        '''
+        """
         Cancelacion de XML firmado por el cliente
         Args:
             xml (str): xml de cfdi firmado por cliente
-        '''
+        """
         try:
             req, conn = self.__setup_req('ns0:CancelacionCFDIRequest')
-            req.TipoPeticion = '1' # SIGNED BY CUSTOMER
+            req.TipoPeticion = '1'  # SIGNED BY CUSTOMER
             req.Emisor = self.config['RFC']
             req.Xml = xml
             self.logger.debug(
